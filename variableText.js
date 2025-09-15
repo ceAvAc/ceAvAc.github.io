df9559bfb5006d5a60d5080c143f179dde9603c2
ceAvAc <ceavemtpa@gmail.com>
2025-09-15 16:04:44 +0300

    Hello world

---
+++ variableText.js
@@ -0,0 +1,215 @@
+//////////////////////////////////////////////
+// Variabelele-tpa 
+//////////////////////////////////////////////
+
+let l1ex="Discurs 1/2 A4 ''Ce inseamna limba rom. pentru mine''";
+let l2ex="Ex. 18-20 p.13"
+let l3ex="-----";
+let l4ex="Ex.4-7 p.10";
+let l5ex="-----";
+let l6ex="De inv. verbele-excepții";
+//let l7ex="";
+
+let m1ex="-----";
+let m2ex="-----";
+let m3ex="Ex.5 p. 16, de înv.";
+let m4ex="De clasificat substanțele, de înv.";
+let m5ex="Ex.5 p. 17, de înv. noțiunele din prezentare";
+let m6ex="De înv. tema";
+let m7ex="-----";
+
+
+let mr1ex="-----";
+let mr2ex="-----";
+let mr3ex="-----";
+let mr4ex="-----";
+let mr5ex="-----";
+let mr6ex="-----";
+let mr7ex="-----";
+
+let j1ex="-----";
+let j2ex="-----";
+let j3ex="-----";
+let j4ex="-----";
+let j5ex="-----";
+let j6ex="-----";
+//let j7ex="";
+
+let v1ex="-----";
+let v2ex="-----";
+let v3ex="-----";
+let v4ex="-----";
+let v5ex="-----";
+let v6ex="-----";
+//let v7ex="";
+
+
+//////////////////////////////////////////////
+// Variabelele-orar (nu schimbati fara nevoie)
+//////////////////////////////////////////////
+
+let l1o="L.rom";
+let l2o="Matematica";
+let l3o="Ed.fiz.";
+let l4o="L.engl";
+let l5o="Biologie";
+let l6o="L.fran (gr.1)"
+//let l7o="";
+
+let m1o="Ed.tehn(gr.1)/Inf(gr.2)";
+let m2o="Ed.tehn(gr.2)/Inf(gr.1)";
+let m3o="Matematica";
+let m4o="Chimia";
+let m5o="L.rom";
+let m6o="Istoria";
+let m7o="L.fran (gr.2)"
+
+let mr1o="Ed.muz";
+let mr2o="DP";
+let mr3o="Biologie";
+let mr4o="L.rom";
+let mr5o="Fizica";
+let mr6o="Opțional";
+let mr7o="L.fran (gr.1)";
+
+let j1o="Istoria";
+let j2o="Geografie";
+let j3o="EpS";
+let j4o="L.rom";
+let j5o="Ed.fiz";
+let j6o="Matematica";
+//let j7o="";
+
+let v1o="L.engl";
+let v2o="Matematica";
+let v3o="Fizica";
+let v4o="L.rom";
+let v5o="Chimie";
+let v6o="L.fran (gr.2)";
+//let v7o="";
+
+let s1o="-----";
+let s2o="-----";
+let s3o="-----";
+let s4o="-----";
+let s5o="-----";
+let s6o="-----";
+let s7o="-----";
+
+//////////////////////////////////////////////
+//Transferarea variabelelor-orar in fileul html
+//////////////////////////////////////////////
+
+//LUNI
+
+document.getElementById("l1o").textContent = l1o;
+document.getElementById("l2o").textContent = l2o;
+document.getElementById("l3o").textContent = l3o;
+document.getElementById("l4o").textContent = l4o;
+document.getElementById("l5o").textContent = l5o;
+document.getElementById("l6o").textContent = l6o;
+//document.getElementById("l7o").textContent = l7o;
+
+document.getElementById("l1ex").textContent = l1ex;
+document.getElementById("l2ex").textContent = l2ex;
+document.getElementById("l3ex").textContent = l3ex;
+document.getElementById("l4ex").textContent = l4ex;
+document.getElementById("l5ex").textContent = l5ex;
+document.getElementById("l6ex").textContent = l6ex;
+//document.getElementById("l7ex").textContent = l7ex;
+
+//MARTI
+
+document.getElementById("m1o").textContent = m1o;
+document.getElementById("m2o").textContent = m2o;
+document.getElementById("m3o").textContent = m3o;
+document.getElementById("m4o").textContent = m4o;
+document.getElementById("m5o").textContent = m5o;
+document.getElementById("m6o").textContent = m6o;
+document.getElementById("m7o").textContent = m7o;
+
+document.getElementById("m1ex").textContent = m1ex;
+document.getElementById("m2ex").textContent = m2ex;
+document.getElementById("m3ex").textContent = m3ex;
+document.getElementById("m4ex").textContent = m4ex;
+document.getElementById("m5ex").textContent = m5ex;
+document.getElementById("m6ex").textContent = m6ex;
+document.getElementById("m7ex").textContent = m7ex;
+
+
+//MIERCURI
+
+document.getElementById("mr1o").textContent = mr1o;
+document.getElementById("mr2o").textContent = mr2o;
+document.getElementById("mr3o").textContent = mr3o;
+document.getElementById("mr4o").textContent = mr4o;
+document.getElementById("mr5o").textContent = mr5o;
+document.getElementById("mr6o").textContent = mr6o;
+document.getElementById("mr7o").textContent = mr7o;
+
+document.getElementById("mr1ex").textContent = mr1ex;
+document.getElementById("mr2ex").textContent = mr2ex;
+document.getElementById("mr3ex").textContent = mr3ex;
+document.getElementById("mr4ex").textContent = mr4ex;
+document.getElementById("mr5ex").textContent = mr5ex;
+document.getElementById("mr6ex").textContent = mr6ex;
+document.getElementById("mr7ex").textContent = mr7ex;
+
+
+//JOI
+
+document.getElementById("j1o").textContent = j1o;
+document.getElementById("j2o").textContent = j2o;
+document.getElementById("j3o").textContent = j3o;
+document.getElementById("j4o").textContent = j4o;
+document.getElementById("j5o").textContent = j5o;
+document.getElementById("j6o").textContent = j6o;
+//document.getElementById("j7o").textContent = j7o;
+
+document.getElementById("j1ex").textContent = j1ex;
+document.getElementById("j2ex").textContent = j2ex;
+document.getElementById("j3ex").textContent = j3ex;
+document.getElementById("j4ex").textContent = j4ex;
+document.getElementById("j5ex").textContent = j5ex;
+document.getElementById("j6ex").textContent = j6ex;
+//document.getElementById("j7ex").textContent = j7ex;
+
+//VINERI
+
+document.getElementById("v1o").textContent = v1o;
+document.getElementById("v2o").textContent = v2o;
+document.getElementById("v3o").textContent = v3o;
+document.getElementById("v4o").textContent = v4o;
+document.getElementById("v5o").textContent = v5o;
+document.getElementById("v6o").textContent = v6o;
+//document.getElementById("v7o").textContent = v7o;
+
+document.getElementById("v1ex").textContent = v1ex;
+document.getElementById("v2ex").textContent = v2ex;
+document.getElementById("v3ex").textContent = v3ex;
+document.getElementById("v4ex").textContent = v4ex;
+document.getElementById("v5ex").textContent = v5ex;
+document.getElementById("v6ex").textContent = v6ex;
+//document.getElementById("v7ex").textContent = v7ex;
+
+//SCHIMBARI
+
+document.getElementById("s1o").textContent = s1o;
+document.getElementById("s2o").textContent = s2o;
+document.getElementById("s3o").textContent = s3o;
+document.getElementById("s4o").textContent = s4o;
+document.getElementById("s5o").textContent = s5o;
+document.getElementById("s6o").textContent = s6o;
+document.getElementById("s7o").textContent = s7o;
+
+//////////////////////////////////////////////
+//Script pentru menu-hamburger
+//////////////////////////////////////////////
+
+const hamMenu= document.querySelector(".ham-menu");
+const dropingMenu = document.querySelector(".dropingMenu");
+
+hamMenu.addEventListener( 'click', () => {
+dropingMenu.classList.toggle('active');
+
+} )
